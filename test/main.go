40d766c8a9b022d6de727bc5318a344162852abf
Lasan Nishshanka <19279535+las-nish@users.noreply.github.com>
2024-07-06 11:40:46 +0530

    Update test content

--- test/main.go
+++ test/main.go
@@ -11,24 +11,22 @@ func main() {
 	// Set log file destination
 	logFilePath := "application"
 	logger.SetFileOutput(logFilePath)
-	logger.SetTerminalOutput(false)
+	logger.SetTerminalOutput(true)
 
 	// Set log formats
 	logger.SetFileFormat(golog.JSONFormat)
-	logger.SetTerminalFormat(golog.JSONFormat)
+	logger.SetTerminalFormat(golog.ReadableFormat)
 
 	// Set log rotation
 	logger.SetLogRotateByCount(10)
 
-	// Set log outputs
-
 	// Log messages
 	for i := 0; i < 20; i++ {
-		logger.Log(golog.NormalLog, map[string]any{"user": "Alice"}, "This is an info message", "This is another info message")
-		logger.Log(golog.ErrorLog, map[string]any{"error": "file not found"}, "This is an error message")
-		logger.Log(golog.SuccessLog, map[string]any{"transaction": "TX12345"}, "This is a success message")
-		logger.Log(golog.WarningLog, map[string]any{"memory": "low"}, "This is a warning message")
-		logger.Log(golog.DebugLog, map[string]any{"details": "debugging app"}, "This is a debug message")
-		logger.Log(golog.InfoLog, nil, "%d This is another info message", 1, 10, 10.5, 'c', true, "aa")
+		logger.Log(golog.NormalLog, map[string]any{"user": "Alice"}, "User login attempt", "Login attempt successful for user: Alice")
+		logger.Log(golog.ErrorLog, map[string]any{"error": "file not found", "path": "/var/www/html/index.html"}, "File not found error", "Attempt to access non-existent file: /var/www/html/index.html")
+		logger.Log(golog.SuccessLog, map[string]any{"transaction": "TX12345", "amount": 150.75}, "Transaction successful", "Transaction ID TX12345 completed successfully with amount $150.75")
+		logger.Log(golog.WarningLog, map[string]any{"memory": "low", "server_id": "srv-01"}, "Low memory warning", "Server srv-01 is experiencing low memory conditions")
+		logger.Log(golog.DebugLog, map[string]any{"details": "request processing", "request_id": "req-67890"}, "Debugging request", "Processing request ID req-67890 for debugging purposes")
+		logger.Log(golog.InfoLog, nil, "150 concurrent users currently online")
 	}
 }
